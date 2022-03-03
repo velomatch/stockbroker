2306d6dad85bb4047cab06ae061502a17e9ca8f7
Aswinikumar <aswini.reddithala@velomatch.in>
2022-03-03 14:32:04 +0530

    moving to PC

--- bespoke_livestock.py
+++
@@ -1,8 +0,0 @@
-from Filereader import FileDataReader
-from LiveStockUpdate import LiveStockUpdate
-
-FileReader = FileDataReader()
-UpdateStock = LiveStockUpdate()
-datafilepath = 'data/Bike Stock Levels Bespoke.csv'
-BespokeData,exceptions = FileReader.readcsvdata(datafilepath)
-UpdateStock.Updatelivestock(BespokeData[:3])
--- concept_livestock.py
+++
@@ -1,8 +0,0 @@
-from Filereader import FileDataReader
-from LiveStockUpdate import LiveStockUpdate
-
-FileReader = FileDataReader()
-UpdateStock = LiveStockUpdate()
-datafilepath = 'data/ruislipStock.xlsx'
-ConceptData,exceptions = FileReader.readxlsxdata(datafilepath)
-UpdateStock.Updatelivestock(ConceptData[:3])
